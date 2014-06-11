e9579cb5fb806e1b939bde38b5768937ef9a321d
Maxime Kohlhaas <maxime@atm-consulting.fr>
2014-06-11 08:46:08 +0200

    Ajout shipping method label

--- expedition.php
+++ expedition.php
@@ -179,6 +179,10 @@ if(isset($_REQUEST['action']) && $_REQUEST['action']=='GENODT') {
 	}
 	
 	$TVA = TODTDocs::getTVA($exp);
+	
+	$code = $langs->getLabelFromKey($db,$expedition->shipping_method_id,'c_shipment_mode','rowid','code');
+	$expedition->shipping_method_label = $langs->trans("SendingMethod".strtoupper($code));
+	
 	$generatedfilename = dol_sanitizeFileName($exp->ref).'-'.$_REQUEST['modele'];
 	if($conf->global->ODTDOCS_FILE_NAME_AS_OBJECT_REF) {
 		$generatedfilename = dol_sanitizeFileName($exp->ref);
